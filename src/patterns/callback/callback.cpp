b6a21398c32d4df58e79d9d29e9b42ff605377d7
Konstantin <konstantin.speransky@yahoo.com>
2016-01-14 18:10:32 +0400

    ADD: callbacks

--- src/patterns/callback/callback.cpp
+++ src/patterns/callback/callback.cpp
@@ -1,9 +1,10 @@
+//http://stackoverflow.com/questions/2298242/callback-functions-in-c
+
 
 #include "callback.h"
 #include <iostream>
 
-std::cout << "FUNCTION CALLBACK:" << std::endl;
-
+//FUNCTION CALLBACK:
 
 int Foo(int x){
 	return x + 2;
@@ -19,13 +20,27 @@ int UseCallback_typedef(int x, Foo_t f){
 	return f(x) + 1000;
 }
 
+//TEMPLATED CALLBACK TYPE:
+
+template<typename F>
+int UseCallback_template(int x, F f){
+	return f(x) + 1000000;
+}
+
 void CallBackTest(){
+
+	std::cout << "FUNCTION CALLBACK:" << std::endl;
 
 	std::cout << "UseCallback(): " << UseCallback(1, Foo) << std::endl;                   //UseCallback(): 13
 	std::cout << "UseCallback(): " << UseCallback(1, &Foo) << std::endl;                  //same meaning, UseCallback(): 13
 
 	std::cout << "UseCallback_typedef(): " << UseCallback_typedef(5, Foo) << std::endl;   //UseCallback_typedef(): 1007
 	std::cout << "UseCallback_typedef(): " << UseCallback_typedef(5, &Foo) << std::endl;  //same meaning, UseCallback_typedef(): 1007
+
+	std::cout << "TEMPLATED CALLBACK TYPE:" << std::endl;
+
+	std::cout << "UseCallback_template: " << UseCallback_template(7, Foo) << std::endl;   //UseCallback_template: 1000009
+
 
 }
 
